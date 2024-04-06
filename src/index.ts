7ab573cb62e940ffad9497f011dd827a92b3a2a7
Keegan Donley <kd@keegandonley.com>
2024-04-06 11:25:48 -0500

    add user agent utils

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./style";
 export * from "./date";
 export * from "./deployment";
+export * from "./ua";
---
+++ src/ua.ts
@@ -0,0 +1,17 @@
+export const mobileUAPatterns = [
+  /Android/i,
+  /webOS/i,
+  /iPhone/i,
+  /iPad/i,
+  /iPod/i,
+  /BlackBerry/i,
+  /Windows Phone/i,
+];
+
+// Normally detecting a user agent like this is not a good idea, but can be treated as a
+// simple performance optimization
+export const getIsLikelyMobile = (userAgent?: string) => {
+  return mobileUAPatterns.some((toMatchItem) => {
+    return userAgent ? userAgent.match(toMatchItem) : false;
+  });
+};
